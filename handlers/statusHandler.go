a26e25e2844d1faeab7e8fc593b52e51a265fd67
sandesol <sandesol@stud.ntnu.no>
2025-02-23 10:01:04 +0100

    Improved statusHandler

--- handlers/statusHandler.go
+++ handlers/statusHandler.go
@@ -24,47 +24,40 @@ func InitializeUptime() {
 
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
 
-	status := &Status{}
-
-	// CountriesNow url
-	urlCN := consts.COUNTRIESNOWURL + "countries/population/cities"
+	urlNOW := consts.COUNTRIESNOWURL + "countries/population/cities"
 
 	payload := strings.NewReader(`{
 		"city": "oslo"
 	}`)
 
 	// Makes a post request and handles errors. Defers closing the body response.
-	respCN, errCN := http.Post(urlCN, "application/json", payload)
-	if errCN != nil {
-
-		// -- TODO --
-
-		fmt.Println(errCN.Error()) // debug
+	respNOW, errNOW := http.Post(urlNOW, "application/json", payload)
+	if errNOW != nil {
+		fmt.Println("Error in POST request: ", errNOW.Error()) // debug
+		return
 	}
-	defer respCN.Body.Close()
+	defer respNOW.Body.Close()
 
-	// REST Countries url
-	urlRC := consts.RESTCOUNTRIESURL + "capital/oslo"
+	urlREST := consts.RESTCOUNTRIESURL + "capital/oslo"
 
 	// Makes a get request and handles errors, similarly to CountriesNow
-	respRC, errRC := http.Get(urlRC)
+	respREST, errRC := http.Get(urlREST)
 	if errRC != nil {
-
-		// TODO: handle error
-
-		fmt.Println(errRC.Error()) // debug
+		fmt.Println("Error in GET request: ", errRC.Error()) // debug
+		return
 	}
-	defer respRC.Body.Close()
+	defer respREST.Body.Close()
 
-	status.CountriesNowStatus = respCN.Status
-	status.RESTCountriesStatus = respRC.Status
+	status := &Status{}
+	status.CountriesNowStatus = respNOW.Status
+	status.RESTCountriesStatus = respREST.Status
 	status.Version = "v1"
 	status.Uptime = time.Now().Unix() - UptimeStart
 
 	// Pretty-prints json from struct
 	jsonStatus, errjson := json.MarshalIndent(status, "", "    ")
 	if errjson != nil {
-		fmt.Printf("Error: %s", errjson.Error())
+		fmt.Println("Error: ", errjson.Error())
 	}
 	fmt.Fprint(w, string(jsonStatus))
 }
